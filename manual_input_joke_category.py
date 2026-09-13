fd332c962c50133b664b58e61abbabcea641fdf5
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-09-13 12:36:50 +0300

    code refactor

--- manual_input_joke_category.py
+++ manual_input_joke_category.py
@@ -9,38 +9,28 @@ class TestNewJoke:
 
         # Получаем категорию от пользователя
         category = input("Введите категорию шутки: ")
-        print(f"Выбранная категория: {category}")
 
         # Получаем список всех доступных категорий
         categories_url = "https://api.chucknorris.io/jokes/categories"
-        print(f"URL на получение категорий: {categories_url}")
 
         # Проверяем, что запрос категорий выполнен успешно
         categories_result = requests.get(categories_url)
-        print(f"Статус код запроса категорий: {categories_result.status_code}")
-
         assert categories_result.status_code == 200
-        print("Категории запрошены успешно")
 
         # Получаем информацию о категориях
         categories = categories_result.json()
-        print(f"Полученные категории: {categories}")
-        print(f"Количество категорий: {len(categories)}")
 
-        # Проверяем, что выбранная пользователем категория есть в списке
-        assert category in categories
-        print("Категория пользователя найдена")
+        # Проверяем, что выбранная пользователем категория есть на сайте
+        if category not in categories:
+            print(f"Категория '{category}' не найдена. Попробуйте ввести другую категорию.")
+            return
 
         url = f"https://api.chucknorris.io/jokes/random?category={category}"
-        print(f"Текущая категория: {category}")
-        print(f"URL категории: {url}")
 
         # Проверяем, что шутка получена успешно
         result = requests.get(url)
-        print(f"Статус код запроса: {result.status_code}")
 
         assert result.status_code == 200
-        print("Шутки получена успешно")
 
         # Кодируем ответ в utf-8
         result.encoding = "utf-8"
@@ -48,12 +38,10 @@ class TestNewJoke:
         # Получаем категории из ответа
         check = result.json()
         check_info = check.get("categories")
-        print(f"Категория в ответе: {check_info}")
 
         # Проверяем, что запрошенная категория есть в ответе
         assert category in check_info
-        print("Категория в ответе корректная")
 
         # Получаем текст шутки
         check_info_value = check.get("value")
-        print(f"Шутка про Чака Норриса: {check_info_value}")
+        print(f"Шутка про Чака Норриса из категории {category}: {check_info_value}")
