ba1619a4d2f105a991acac5fa7176c609240ee8b
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-09-15 18:54:38 +0300

    code refactoring

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,3 +1,5 @@
+import json
+
 from utils.api import GoogleMapsAPI
 from utils.checking import Checking
 
@@ -7,29 +9,40 @@ from utils.checking import Checking
 class TestCreatePlace:
 
     def test_create_new_place(self):
-
         print("Method POST")
         result_post = GoogleMapsAPI.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print("Method GET")
         result_get = GoogleMapsAPI.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
 
         print("Method PUT")
         result_put = GoogleMapsAPI.update_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Method GET after update")
         result_get = GoogleMapsAPI.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
 
         print("Method DELETE")
         result_delete = GoogleMapsAPI.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
 
         print("Method GET after delete")
         result_get = GoogleMapsAPI.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+
+        print("test_create_new_place PASSED")
--- utils/checking.py
+++ utils/checking.py
@@ -1,10 +1,20 @@
-"""Methods for assert responce"""
+import json
+
+"""Methods for assert response"""
 
 
 class Checking:
-    """Method for check status code"""
 
     @staticmethod
     def check_status_code(result, status_code):
+        """Method for check status code"""
         assert status_code == result.status_code, 'Error, stratus code not assert'
         print(f"Success! Status code = {result.status_code}")
+
+    @staticmethod
+    def check_json_token(result, expected_value):
+        """Method for check json in response"""
+        fields = json.loads(result.text)
+        assert list(fields) == expected_value, 'Error, json have not required fields'
+        print(list(fields))
+        print("All fields are present")
